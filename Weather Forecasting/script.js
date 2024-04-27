0590a1c36e968f57564fa57c541a3de73adc05a9
Shubhendra Singh <shubhendra.ss2002@gmail.com>
2024-04-27 12:33:49 +0530

    Add files via upload

---
+++ Weather Forecasting/script.js
@@ -0,0 +1,24 @@
+function getWeather() {
+    const city = document.getElementById('cityInput').value;
+    const apiKey = 'f302d1cceaa3b48035dff6cbbbea7c7a'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.log('Error fetching weather data:', error);
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].main}</p>
+        <p>Description: ${data.weather[0].description}</p>
+    `;
+}
